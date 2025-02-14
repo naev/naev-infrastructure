44c04cc74574aedb7fd518e0fb492faff2c9caaa
Jack Greiner <jack@emoss.org>
2025-02-14 11:17:38 -0500

    Remove comments and whitespace

--- naev-windows/setup-devenv.sh
+++ naev-windows/setup-devenv.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
-# Set HOST and MACOSX_DEPLOYMENT_TARGET depending on which BUILD_ARCH is selected.
-# Also configure the macports directory for the right libraries since we build for two different targets.
-
-# Pass in either BUILDARCH=aarch64 or BUILDARCH=x86_64 when starting this image to configure it correctly.
-
 # Print a message 
 echo "Configuring the development environment"
 
-
 rustup-init -y -t x86_64-pc-windows-gnu
-
 
 # Source the environment file
 source ~/.cargo/env
